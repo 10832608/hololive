3c0bdf18b645d720904b1017690fcaba0b58a4db
10832608 <76466507+10832608@users.noreply.github.com>
2021-06-22 11:54:00 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://10832608.github.io/imageai2/';
+let imageModelURL = 'https://10832608.github.io/hololive/';
 
 // Video
 
